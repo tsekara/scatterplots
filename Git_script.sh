2d692e33ec7980f4be7c4c815cd7c6177d178034
tsekara <sekaran@mac-hentze50.local>
2019-07-26 13:31:58 +0200

    Gits_script is added

---
+++ Git_script.sh
@@ -0,0 +1,9 @@
+#### Pusing the directory to GIT
+
+#first create the repository in the GIT using the same Rproject name
+#Later go the recpective folder and sync it with using
+
+git remote add origin git@github.com:tsekara/scatterplots.git
+git add * # add all the contents to Github
+git commit -m "First commit
+git push -u origin master
